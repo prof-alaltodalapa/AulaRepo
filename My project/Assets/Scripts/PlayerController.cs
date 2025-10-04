f333c106f81c3803045897e51c38253d1372ccb2
prof-alaltodalapa <profal.altodalapa@gmail.com>
2025-10-04 16:45:13 -0300

    04 de Outubro - Pos Rep
    04 de Outubro - Pos Rep

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -7,19 +7,45 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rig;
     public Animator anim;
     public float speed = 10;
+    public float attackDelay = 1.2f;
+    public float dealDamageDelay = 0.5f;
+
+    private bool locked = false;
 
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-
-        rig.velocity = new Vector2(x, y).normalized * speed;
-        anim.SetBool("IsRunning", x != 0f || y != 0f);
-        
-        if(anim.GetBool("IsRunning"))
+        if(!locked)
         {
-            anim.SetFloat("X", x > 0f ? 1f : x < 0f ? -1f : 0f);
-            anim.SetFloat("Y", y > 0f ? 1f : y < 0f ? -1f : 0f);
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+
+            rig.velocity = new Vector2(x, y).normalized * speed;
+            anim.SetBool("IsRunning", x != 0f || y != 0f);
+            
+            if(anim.GetBool("IsRunning"))
+            {
+                anim.SetFloat("X", x > 0f ? 1f : x < 0f ? -1f : 0f);
+                anim.SetFloat("Y", y > 0f ? 1f : y < 0f ? -1f : 0f);
+            }
+
+            if(Input.GetMouseButtonDown(0))
+            {
+                anim.SetTrigger("Attack");
+                rig.velocity = new Vector2(0, 0);
+
+                locked = true;
+                CancelInvoke("Unlock");
+                Invoke("Unlock", attackDelay);
+                CancelInvoke("DealDamage");
+                Invoke("DealDamage", dealDamageDelay);
+            }
         }
     }
+
+    void DealDamage()
+    {
+        //dar dano nos inimigos
+    }
+
+    void Unlock() => locked = false;
 }
