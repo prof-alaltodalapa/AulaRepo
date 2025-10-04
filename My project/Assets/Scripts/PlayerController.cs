7b84141efdcec70ef1dcfa952ef137b7f68eede8
prof-alaltodalapa <profal.altodalapa@gmail.com>
2025-10-04 10:37:18 -0300

    Backup 4 de Outubro
    Backup 4 de Outubro

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -4,26 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     public Rigidbody2D rig;
-
+    public Animator anim;
     public float speed = 10;
 
-    void FixedUpdate()
-    {
-        rig.velocity = new Vector2(
-            Input.GetAxis("Horizontal") * speed,
-            Input.GetAxis("Vertical") * speed);
-    }
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        rig.velocity = new Vector2(x, y).normalized * speed;
+        anim.SetBool("IsRunning", x != 0f || y != 0f);
         
+        if(anim.GetBool("IsRunning"))
+        {
+            anim.SetFloat("X", x > 0f ? 1f : x < 0f ? -1f : 0f);
+            anim.SetFloat("Y", y > 0f ? 1f : y < 0f ? -1f : 0f);
+        }
     }
 }
